a762a3ee8c0bfa3cea2ed82c6fa32866083af395
Alexander Fedorenko <alexander@fedorenko.im>
2015-07-28 15:57:57 +0300

    Add redirection

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,70 +1,38 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
 
-  # GET /links
+  # GET /
   def index
     @link = Link.new
     @links = Link.all
   end
 
-  # GET /links/1
-  # GET /links/1.json
+  # GET /abc
   def show
   end
 
-  # GET /links/new
-  def new
-    @link = Link.new
-  end
-
-  # GET /links/1/edit
-  def edit
-  end
-
-  # POST /links
-  # POST /links.json
+  # POST /
   def create
     @link = Link.new(link_params)
 
     respond_to do |format|
       if @link.save
-        format.html { redirect_to @link, notice: 'Link was successfully created.' }
-        format.json { render :show, status: :created, location: @link }
+        redirect_to @link, notice: 'Link was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
+        render :new
       end
-    end
-  end
-
-  # PATCH/PUT /links/1
-  # PATCH/PUT /links/1.json
-  def update
-    respond_to do |format|
-      if @link.update(link_params)
-        format.html { redirect_to @link, notice: 'Link was successfully updated.' }
-        format.json { render :show, status: :ok, location: @link }
-      else
-        format.html { render :edit }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /links/1
-  # DELETE /links/1.json
-  def destroy
-    @link.destroy
-    respond_to do |format|
-      format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_link
-      @link = Link.find(params[:id])
+      @link = Link.find_by_url(params[:id])
+      if @link.nil?
+        redirect_to root_path
+      else
+        redirect_to @link.origin
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
